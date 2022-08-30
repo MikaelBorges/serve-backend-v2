f1eb0b8f6246baf59043a61a04d4d445cfe0086c
Mikael B <mika4ever@hotmail.fr>
2022-08-30 17:12:39 +0200

    nettoyage

--- server.js
+++ server.js
@@ -97,8 +97,7 @@ mongoose
     userRoutes(app, db)
     annoncesRoutes(app, db)
 
-    app.listen(process.env.PORT || '3306', function() {
-    // app.listen(process.env.PORT, function() {
+    app.listen(process.env.PORT || 3306, function() {
       console.log("serveur prêt")
     });
   })
